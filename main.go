965c9c3c2a584f3bf16842735cbf9058a9dfc0ae
Lars Michaelis <mail@larmic.de>
2021-01-27 09:28:02 +0100

    initial commit

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	log.Println("Hello sonnenBatterie-api!")
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/hello", HelloHandler).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("Hello sonnenBatterie-api!"))
 }
