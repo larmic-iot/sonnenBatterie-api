6f7ab78d65ac5f99a6080c60afcece5b11bee06e
Lars Michaelis <mail@larmic.de>
2021-02-16 16:46:03 +0100

    clean up code

--- api/client/sonnenBatterieAuthClient.go
+++ api/client/sonnenBatterieAuthClient.go
@@ -4,9 +4,14 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"sonnen-batterie-api/api/client/crypto"
+)
+
+const (
+	JsonContentType = "application/json"
 )
 
 type AuthClient struct {
@@ -39,26 +44,26 @@ func (c *AuthClient) GetAuthToken() string {
 	challenge, _ := GetChallenge(c.Ip)
 	encryptedPassword := crypto.Encrypt(c.password, challenge)
 
-	// get token
-	body := `{"user":"` + c.User + `","challenge":"` + challenge + `","response":"` + encryptedPassword + `"}`
+	body := c.buildSessionBody(challenge, encryptedPassword)
 	fmt.Println("POST session, Body: ", body)
-	post, _ := http.Post(baseUrl+"session", "application/json", bytes.NewBuffer([]byte(body)))
+	post, _ := http.Post(baseUrl+"session", JsonContentType, body)
 	defer post.Body.Close()
-	token, _ := ioutil.ReadAll(post.Body)
-	var t Token
-	_ = json.Unmarshal(token, &t)
-	return t.Token
+
+	return unmarshalToken(post.Body).Token
 }
 
-func getSecure(url, token string) string {
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("Auth-Token", token)
-	client := &http.Client{}
-	resp, _ := client.Do(req)
-	body, _ := ioutil.ReadAll(resp.Body)
-	return string(body)
+func (c *AuthClient) buildSessionBody(challenge string, encryptedPassword string) io.Reader {
+	body := `{"user":"` + c.User + `","challenge":"` + challenge + `","response":"` + encryptedPassword + `"}`
+	return bytes.NewBuffer([]byte(body))
 }
 
 func (c *AuthClient) buildChallengeUrl() string {
 	return "http://" + c.Ip + "/api/challenge"
+}
+
+func unmarshalToken(body io.ReadCloser) Token {
+	token, _ := ioutil.ReadAll(body)
+	var t Token
+	_ = json.Unmarshal(token, &t)
+	return t
 }
